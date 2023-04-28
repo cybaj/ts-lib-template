a2affa5c8dd191d42b82a0e9033c154586f8bfa9
cybaj <cybaj@naver.com>
2023-04-28 21:06:41 +0900

    update: jest features

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testEnvironment: "node",
+  preset: "ts-jest",
+  setupFiles: ["./tests/setup.ts"],
+};
---
+++ tests/setup.ts
@@ -0,0 +1,5 @@
+// import {config} from 'dotenv';
+// import * as path from 'path';
+// config({path: path.resolve(__dirname, '../.env')});
+
+console.debug('process.env.NODE_ENV', process.env.NODE_ENV);
